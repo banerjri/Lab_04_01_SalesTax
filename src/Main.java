a4d61e0e2cfb8830db957a38ee92b99c58009906
banerjri <banerjri@mail.uc.edu>
2023-06-16 10:04:12 -0500

    code done

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double price = 30.3;
+        double saleTax = .05;
+        double tax;
+        tax = price * saleTax;
+        System.out.println("The price of the product " + price + " sales tax for the product " +tax);
+
+    }
+}
